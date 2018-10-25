f4cf488cc07aa287d56e458e1184f319c0bad30c
at-hangtran <hang.tran@asiantech.vn>
2018-10-25 14:49:33 +0700

    refactor code

--- src/test/java/vn/asiantech/page/employee/EmployeesPage.java
+++ src/test/java/vn/asiantech/page/employee/EmployeesPage.java
@@ -1,6 +1,9 @@
 package vn.asiantech.page.employee;
 
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import vn.asiantech.base.BasePage;
