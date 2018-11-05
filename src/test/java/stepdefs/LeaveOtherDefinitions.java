9d45f74b4f0df7bead4a7814269b32836075f78e
Hanh Nguyen M <hanh.nguyen2@asiantech.vn>
2018-11-05 11:22:49 +0700

    update branch

--- src/test/java/stepdefs/LeaveOtherDefinitions.java
+++ src/test/java/stepdefs/LeaveOtherDefinitions.java
@@ -1,18 +1,29 @@
 package stepdefs;
 
-import cucumber.api.PendingException;
 import cucumber.api.java8.En;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import vn.asiantech.base.Constant;
 import vn.asiantech.base.DriverBase;
 import vn.asiantech.page.LeaveOtherPage;
 
+/**
+ * @author at-hanhnguyen
+ */
 public class LeaveOtherDefinitions extends DriverBase implements En {
     private LeaveOtherPage leaveOtherPage;
     private WebDriver webDriver;
+
     private static final int TIME_OUT = 30;
+    private static final int EMPLOYER_ID_POS = 0;
+    private static final int EMPLOYER_NAME_POS = 1;
+
+    private String pathOrganisationEmployer;
+    private String pathEmployerName;
+    private int countItem;
 
     public LeaveOtherDefinitions() {
         try {
@@ -22,12 +33,13 @@ public class LeaveOtherDefinitions extends DriverBase implements En {
         }
 
         And("^Open leave of other page$", () -> {
-            webDriver.get("http://portal-stg.asiantech.vn/leave/tracking");
+            webDriver.get(Constant.LEAVE_TRACKING_PAGE_URL);
             leaveOtherPage = initPage(getDriver(), LeaveOtherPage.class);
             new WebDriverWait(webDriver, TIME_OUT).until(
-                    webDriver -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete"));
-            Assert.assertEquals("http://portal-stg.asiantech.vn/leave/tracking", webDriver.getCurrentUrl());
+                    driver -> ((JavascriptExecutor) driver).executeScript("return document.readyState").equals("complete"));
+            Assert.assertEquals(Constant.LEAVE_TRACKING_PAGE_URL, webDriver.getCurrentUrl());
         });
+        And("^Wait for load data$", () -> leaveOtherPage.waitForLoadData(webDriver));
 
         When("^I click status item$", () -> leaveOtherPage.clickStatusItem());
         Then("^Should show status dropdown$", () -> Assert.assertTrue(leaveOtherPage.checkShowStatusDropdown()));
@@ -38,10 +50,35 @@ public class LeaveOtherDefinitions extends DriverBase implements En {
         When("^I click end date$", () -> leaveOtherPage.clickItemEndDate());
         Then("^Should show end day dialog$", () -> Assert.assertTrue(leaveOtherPage.checkShowEndDateDialog()));
 
-
         Then("^I choose a start date$", () -> leaveOtherPage.chooseStartDate());
         Then("^I choose a end date$", () -> leaveOtherPage.chooseEndDate());
         When("^end date less than start date$", () -> Assert.assertTrue(leaveOtherPage.compareTwoDate()));
         Then("^show error message with text is \"([^\"]*)\"$", (String message) -> Assert.assertTrue(leaveOtherPage.checkShowErrorMessage(message)));
+
+        Given("^Leave of other have data$", () -> {
+            Assert.assertFalse(leaveOtherPage.checkDataEmpty());
+            pathOrganisationEmployer = leaveOtherPage.getPathPageRedirect(EMPLOYER_ID_POS);
+            pathEmployerName = leaveOtherPage.getPathPageRedirect(EMPLOYER_NAME_POS);
+            countItem = leaveOtherPage.countDataItem(webDriver);
+        });
+        When("^I click in employer id$", () -> leaveOtherPage.clickEmployerId());
+        Then("^Should redirect to organisation employees$", () -> redirectPage(pathOrganisationEmployer));
+
+        When("^I click in employees name$", () -> leaveOtherPage.clickEmployerName());
+        Then("^Should redirect to profile detail$", () -> redirectPage(pathEmployerName));
+
+        When("^I click in icon search$", () -> leaveOtherPage.clickSearchIcon());
+
+        When("^Button next_to_end_page is shown$", () -> Assert.assertTrue(leaveOtherPage.checkButtonNextEndPageShown(countItem)));
+        And("^I click button next_to_end_page$", () -> leaveOtherPage.clickButtonLast());
+        Then("^Should show end page$", () -> Assert.assertEquals(getDriver().getCurrentUrl(), leaveOtherPage.getPathLastPage(countItem)));
+    }
+
+    private void redirectPage(final String path) {
+        new WebDriverWait(getDriver(), TIME_OUT).until(
+                driver -> driver.findElement(By.className("col-sm-8")).findElement(By.tagName("h2")).isDisplayed()
+        );
+        String url = getDriver().getCurrentUrl();
+        Assert.assertEquals(path, url.substring(url.length() - path.length()));
     }
 }
--- src/test/java/vn/asiantech/page/LeaveOtherPage.java
+++ src/test/java/vn/asiantech/page/LeaveOtherPage.java
@@ -1,84 +1,188 @@
 package vn.asiantech.page;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import vn.asiantech.base.BasePage;
+import vn.asiantech.base.Constant;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-public class LeaveOtherPage {
+/**
+ * @author at-hanhnguyen
+ */
+public class LeaveOtherPage extends BasePage<LeaveOtherPage> {
+
+    @FindBy(css = ".ibox-content.main-content")
+    private WebElement iboxContent;
     @FindBy(className = "dropdown-sm")
     private WebElement statusItem;
     @FindBy(className = "dropdown-md")
     private WebElement groupByItem;
-    @FindBy(name = "start")
-    private WebElement itemDateStart;
-    @FindBy(name = "end")
-    private WebElement itemDateEnd;
+    @FindBy(className = "dropdown-lg")
+    private WebElement itemTime;
     @FindBy(id = "page-wrapper")
     private WebElement mainBody;
+    @FindBy(css = ".ui-datatable.ui-widget.ui-datatable-scrollable")
+    private WebElement tableData;
+    @FindBy(css = ".ui-widget-content.ui-datatable-emptymessage-row.ng-star-inserted")
+    private WebElement trEmptyMessage;
+    @FindBy(css = ".ui-paginator-last.ui-paginator-element.ui-state-default.ui-corner-all")
+    private WebElement btnLast;
 
-    private static final String FORMAT_SLASH_MARK = "yyyy/MM/dd";
+    private static final String FORMAT_SLASH_MARK = "MMM dd, yyyy";
+    private static final int TIME_SLEEP = 300; // millisecond
+    private static final int TIME_SLEEP_CLICK = 1000; // millisecond
 
-    public LeaveOtherPage() {
-        //no-up
+    private static final int CALENDAR_START_POS = 0;
+    private static final int CALENDAR_END_POS = 1;
+    private static final int LINE_OF_CALENDAR_POS = 3;
+    private static final int CALENDAR_START_ITEM_CLICK_POS = 5;
+    private static final int CALENDAR_END_ITEM_CLICK_POS = 3;
+
+    private static final int FIRST_ITEM_POS = 0;
+    private static final int EMPLOYER_ID_POS = 0;
+    private static final int EMPLOYER_NAME_POS = 1;
+    private static final int ICON_SEARCH_POS = 8;
+
+    private static final int MAX_ITEM_ON_A_PAGE = 50;
+    private static final int MAX_PAGE_COUNT_ON_BOTTOM = 5;
+
+    @Override
+    public final LeaveOtherPage navigateTo(final WebDriver webDriver) {
+        return this;
     }
 
-    public void clickStatusItem() {
+    public final void waitForLoadData(final WebDriver driver) {
+        waitForElement(driver, iboxContent);
+    }
+
+    public final void clickStatusItem() {
         statusItem.click();
     }
 
-    public boolean checkShowStatusDropdown() {
+    public final boolean checkShowStatusDropdown() {
         return statusItem.findElement(By.cssSelector(".ui-dropdown-panel.ui-widget-content.ui-corner-all.ui-shadow.ng-trigger.ng-trigger-panelState")).isDisplayed();
     }
 
-    public void clickGroupByItem() {
+    public final void clickGroupByItem() {
         groupByItem.click();
     }
 
-    public boolean checkShowGroupByItem() {
+    public final boolean checkShowGroupByItem() {
         return groupByItem.findElement(By.cssSelector(".ui-dropdown-panel.ui-widget-content.ui-corner-all.ui-shadow.ng-trigger.ng-trigger-panelState")).isDisplayed();
     }
 
-    public void clickItemStartDate() {
-        itemDateStart.click();
+    public final void clickItemStartDate() {
+        itemTime.findElements(By.tagName("p-calendar")).get(CALENDAR_START_POS).click();
     }
 
-    public void clickItemEndDate() {
-        itemDateEnd.click();
+    public final void clickItemEndDate() {
+        itemTime.findElements(By.tagName("p-calendar")).get(CALENDAR_END_POS).click();
     }
 
-    public boolean checkShowStartDateDialog() {
-        return itemDateStart.findElement(By.cssSelector(".ui-datepicker.ui-widget.ui-widget-content.ui-helper-clearfix.ui-corner-all.ui-shadow.ng-trigger.ng-trigger-overlayState")).isDisplayed();
+    public final boolean checkShowStartDateDialog() {
+        return itemTime.findElements(By.tagName("p-calendar")).get(CALENDAR_START_POS).findElement(By.cssSelector(".ui-datepicker.ui-widget.ui-widget-content.ui-helper-clearfix.ui-corner-all.ui-shadow.ng-trigger.ng-trigger-overlayState")).isDisplayed();
     }
 
-    public boolean checkShowEndDateDialog() {
-        return itemDateEnd.findElement(By.cssSelector(".ui-datepicker.ui-widget.ui-widget-content.ui-helper-clearfix.ui-corner-all.ui-shadow.ng-trigger.ng-trigger-overlayState")).isDisplayed();
+    public final boolean checkShowEndDateDialog() {
+        return itemTime.findElements(By.tagName("p-calendar")).get(CALENDAR_END_POS).findElement(By.cssSelector(".ui-datepicker.ui-widget.ui-widget-content.ui-helper-clearfix.ui-corner-all.ui-shadow.ng-trigger.ng-trigger-overlayState")).isDisplayed();
     }
 
-    public void chooseStartDate() {
-        itemDateStart.findElement(By.cssSelector(".ui-datepicker-calendar.ng-star-inserted")).findElement(By.tagName("tbody")).findElements(By.tagName("tr")).get(4).findElements(By.tagName("td")).get(5).click();
+    public final void chooseStartDate() {
+        itemTime.findElements(By.tagName("p-calendar")).get(CALENDAR_START_POS).findElement(By.cssSelector(".ui-datepicker-calendar.ng-star-inserted")).findElement(By.tagName("tbody")).findElements(By.tagName("tr")).get(LINE_OF_CALENDAR_POS).findElements(By.tagName("td")).get(CALENDAR_START_ITEM_CLICK_POS).click();
     }
 
-    public void chooseEndDate() {
-        itemDateEnd.findElement(By.cssSelector(".ui-datepicker-calendar.ng-star-inserted")).findElement(By.tagName("tbody")).findElements(By.tagName("tr")).get(4).findElements(By.tagName("td")).get(3).click();
+    public final void chooseEndDate() {
+        itemTime.findElements(By.tagName("p-calendar")).get(CALENDAR_END_POS).findElement(By.cssSelector(".ui-datepicker-calendar.ng-star-inserted")).findElement(By.tagName("tbody")).findElements(By.tagName("tr")).get(LINE_OF_CALENDAR_POS).findElements(By.tagName("td")).get(CALENDAR_END_ITEM_CLICK_POS).click();
     }
 
-    public boolean compareTwoDate() {
-        Date startDate = convertStringToDate(itemDateStart.findElement(By.cssSelector(".ui-inputtext.ui-widget.ui-state-default.ui-corner-all.ng-star-inserted")).getText());
-        Date endDate = convertStringToDate(itemDateEnd.findElement(By.cssSelector(".ui-inputtext.ui-widget.ui-state-default.ui-corner-all.ng-star-inserted")).getText());
+    public final boolean compareTwoDate() {
+        Date startDate = convertStringToDate(itemTime.findElements(By.tagName("p-calendar")).get(CALENDAR_START_POS).findElement(By.cssSelector(".ui-inputtext.ui-widget.ui-state-default.ui-corner-all.ng-star-inserted")).getAttribute("value"));
+        Date endDate = convertStringToDate(itemTime.findElements(By.tagName("p-calendar")).get(CALENDAR_END_POS).findElement(By.cssSelector(".ui-inputtext.ui-widget.ui-state-default.ui-corner-all.ng-star-inserted")).getAttribute("value"));
         return startDate != null && endDate != null && startDate.compareTo(endDate) > 0;
     }
 
-    public boolean checkShowErrorMessage(String message) {
+    public final boolean checkShowErrorMessage(final String message) {
         WebElement errorMessage = mainBody.findElement(By.tagName("app-error")).findElement(By.cssSelector(".app-alert.ng-star-inserted"));
-        return errorMessage.isDisplayed() && errorMessage.getText().trim().equals(message);
+        return errorMessage.isDisplayed() && errorMessage.getText().trim().contains(message);
     }
 
-    private Date convertStringToDate(String inputDate) {
+    public final boolean checkDataEmpty() {
+        return isElementPresented(trEmptyMessage);
+    }
+
+    public final void clickEmployerId() {
+        WebElement itemFirst = tableData.findElement(By.cssSelector(".ui-datatable-data.ui-widget-content")).findElements(By.tagName("tr")).get(FIRST_ITEM_POS);
+        WebElement employerId = itemFirst.findElements(By.tagName("td")).get(EMPLOYER_ID_POS);
+        employerId.findElement(By.className("ng-star-inserted")).click();
+        try {
+            Thread.sleep(TIME_SLEEP_CLICK);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public final String getPathPageRedirect(final Integer position) {
+        WebElement itemFirst = tableData.findElement(By.cssSelector(".ui-datatable-data.ui-widget-content")).findElements(By.tagName("tr")).get(FIRST_ITEM_POS);
+        WebElement employerId = itemFirst.findElements(By.tagName("td")).get(position);
+        return employerId.findElement(By.tagName("span")).findElement(By.tagName("a")).getAttribute("href");
+    }
+
+    public final void clickEmployerName() {
+        WebElement itemFirst = tableData.findElement(By.cssSelector(".ui-datatable-data.ui-widget-content")).findElements(By.tagName("tr")).get(FIRST_ITEM_POS);
+        WebElement employerName = itemFirst.findElements(By.tagName("td")).get(EMPLOYER_NAME_POS);
+        employerName.findElement(By.className("ng-star-inserted")).findElement(By.className("info-grouping")).click();
+        try {
+            Thread.sleep(TIME_SLEEP_CLICK);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public final void clickSearchIcon() {
+        WebElement itemFirst = tableData.findElement(By.cssSelector(".ui-datatable-data.ui-widget-content")).findElements(By.tagName("tr")).get(FIRST_ITEM_POS);
+        WebElement searchIcon = itemFirst.findElements(By.tagName("td")).get(ICON_SEARCH_POS);
+        searchIcon.findElement(By.cssSelector(".view.ng-star-inserted")).click();
+        try {
+            Thread.sleep(TIME_SLEEP_CLICK);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public final int countDataItem(final WebDriver driver) {
+        String lineCountPage = driver.findElement(By.cssSelector(".show-entry.ng-star-inserted")).getText();
+        String countPage = lineCountPage.substring(lineCountPage.indexOf("of") + 2, lineCountPage.indexOf("entries"));
+        return Integer.valueOf(countPage.trim());
+    }
+
+    public final boolean checkButtonNextEndPageShown(final int countItem) {
+        return countItem / MAX_ITEM_ON_A_PAGE > MAX_PAGE_COUNT_ON_BOTTOM || (countItem / MAX_ITEM_ON_A_PAGE == MAX_PAGE_COUNT_ON_BOTTOM && countItem % MAX_ITEM_ON_A_PAGE > 0);
+    }
+
+    public final void clickButtonLast() {
+        btnLast.click();
+        try {
+            Thread.sleep(TIME_SLEEP);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public final String getPathLastPage(final int countItem) {
+        int numberPage = countItem / MAX_ITEM_ON_A_PAGE;
+        if (countItem % MAX_ITEM_ON_A_PAGE > 0) {
+            numberPage = numberPage + 1;
+        }
+        return Constant.LEAVE_TRACKING_PAGE_URL + ";page=" + numberPage;
+    }
+
+    private Date convertStringToDate(final String inputDate) {
         SimpleDateFormat sdf = new SimpleDateFormat(FORMAT_SLASH_MARK, Locale.getDefault());
         try {
             return sdf.parse(inputDate);
