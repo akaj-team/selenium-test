c5015e7d531c268b10a407b1733c01360465254e
at-hangtran <hang.tran@asiantech.vn>
2019-01-21 15:23:07 +0700

    fix ip

--- src/test/java/vn/asiantech/base/DriverFactory.java
+++ src/test/java/vn/asiantech/base/DriverFactory.java
@@ -61,7 +61,7 @@ public class DriverFactory {
     private static Map<String, String> defaultParam() {
         Map<String, String> parameters = new HashMap<>();
         parameters.put("browserName", Constant.BROWSER_CHROME);
-        parameters.put("server", "http://localhost:4444/wd/hub");
+        parameters.put("server", "http://192.168.99.1:4444/wd/hub");
         return parameters;
     }
 
