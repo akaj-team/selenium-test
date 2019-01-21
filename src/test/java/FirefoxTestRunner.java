3492173cda878eaf25bdbc1ba07d2e8aa058fb42
at-hangtran <hang.tran@asiantech.vn>
2019-01-21 15:01:50 +0700

    run parallel

--- src/test/java/FirefoxTestRunner.java
+++ src/test/java/FirefoxTestRunner.java
@@ -3,16 +3,16 @@ import org.testng.annotations.Test;
 import vn.asiantech.base.CucumberRunnerBase;
 
 @CucumberOptions(
-        features = "src/test/resources/features",
+        features = "src/test/resources/features/wiki",
         glue = {"stepdefs"},
         tags = {"not @Ignore"},
         plugin = {
                 "pretty",
                 "html:target/cucumber-reports/cucumber-pretty",
-                "json:target/cucumber-reports/CucumberTestReport.json",
+                "json:target/cucumber-reports/FirefoxCucumberTestReport.json",
                 "rerun:target/cucumber-reports/rerun.txt"
         })
 @Test
-public class TestRunner extends CucumberRunnerBase {
+public class FirefoxTestRunner extends CucumberRunnerBase {
 
 }
