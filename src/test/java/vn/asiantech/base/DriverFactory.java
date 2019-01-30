c7172cbdc7c421daebfa063efefc68346c60f08c
at-hangtran <hang.tran@asiantech.vn>
2019-01-30 15:58:18 +0700

    change ip

--- src/test/java/vn/asiantech/base/DriverFactory.java
+++ src/test/java/vn/asiantech/base/DriverFactory.java
@@ -59,7 +59,7 @@ public class DriverFactory {
     private static Map<String, String> defaultParam() {
         Map<String, String> parameters = new HashMap<>();
         parameters.put("browserName", Constant.BROWSER_CHROME);
-        parameters.put("server", "http://172.18.28.144:4444/wd/hub");
+        parameters.put("server", "http://127.0.0.1:4444/wd/hub");
         return parameters;
     }
 
