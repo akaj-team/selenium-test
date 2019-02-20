d63a6385a8f690f83b20cbaf5ffcd214f599af16
at-hangtran <hang.tran@asiantech.vn>
2019-02-20 16:36:44 +0700

    divide test

--- src/test/java/ChromeTestRunner.java
+++ src/test/java/ChromeTestRunner.java
@@ -6,7 +6,7 @@ import vn.asiantech.base.CucumberRunnerBase;
  * ChromeTestRunner
  */
 @CucumberOptions(
-        features = "src/test/resources/features",
+        features = "src/test/resources/features/login",
         glue = {"stepdefs"},
         tags = {"not @Ignore"},
         plugin = {
--- src/test/java/FirefoxTestRunner.java
+++ src/test/java/FirefoxTestRunner.java
@@ -6,7 +6,7 @@ import vn.asiantech.base.CucumberRunnerBase;
  * FirefoxTestRunner
  */
 @CucumberOptions(
-        features = "src/test/resources/features",
+        features = "src/test/resources/features/login",
         glue = {"stepdefs"},
         tags = {"not @Ignore"},
         plugin = {
